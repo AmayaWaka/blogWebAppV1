fd5aa9fd29c113b50d0c436f4632736bb26d3e84
AmayaWaka <rmadegwa63@gmail.com>
2022-09-14 01:49:08 +0300

    Added a for each loop to help loop through the posts array

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-const homeContent =[];
+const posts =[];
 // const postBody =[];
 const aboutContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -21,8 +21,11 @@ app.use(express.static("public"));
 //Routes
 //Home Route
 app.get("/", function(req, res){
-  res.render("home", {title: homeContent.title, bodyContent: homeContent.});
+  res.render("home", {posts: posts});
+
+
 });
+
 
 
 
@@ -45,9 +48,10 @@ const post = {
   title:req.body.newPost,
   content:req.body.blogBody
 };
-homeContent.push(post);
+posts.push(post);
 
-  res.redirect("/compose");
+
+  res.redirect("/");
 
 
 
