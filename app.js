e1dffdc679e28d8f35e9f567f612bca48d7aeed2
AmayaWaka <rmadegwa63@gmail.com>
2022-09-14 03:31:07 +0300

    Trancated my blog content to 100 characters to remove clatter

--- app.js
+++ app.js
@@ -63,7 +63,11 @@ app.get("/posts/:postName", function(req, res){
   posts.forEach(function(post){
     var storedTitle = _.lowerCase(post.title);
     if (storedTitle === requestedTitle){
-      console.log("Match Found");
+      res.render("post", {
+        title: post.title,
+        content: post.content
+
+      })
     }else{
       console.log("No match found");
     }
